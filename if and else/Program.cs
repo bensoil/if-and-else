c199622a823e49372b98fa80dc36a9e40f11f71c
bensoil <157688179+bensoil@users.noreply.github.com>
2024-05-13 13:01:41 +0200

    Add project files.

---
+++ if and else/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Oppgave 1*/
+
+int firstNumber = 5;
+int secondNumber = 4;
+bool sameNumber = false;
+void IsNumber()
+{
+    if (firstNumber == secondNumber)
+    {
+        sameNumber = true;
+        Console.WriteLine("This number is same!!!");
+    }
+    else
+    {
+        sameNumber = false;
+        Console.WriteLine("This number is NOT same!!!");
+    }
+}
+
+IsNumber();
+
+
+/*Oppgave 2*/
+
+
+
+int NumbersHunter()
+{
+    if (firstNumber == secondNumber)
+    {
+        return firstNumber * secondNumber;
+    }
+    else
+    {
+        return firstNumber + secondNumber;
+    }
+}
+
+Console.WriteLine(NumbersHunter());
+
+
+/*Oppgave 3*/
+bool exactSum = false; 
+bool ExactNumber()
+{
+    if (firstNumber + secondNumber == 30)
+    {
+       return exactSum = true;
+    }
+    else
+    {
+       return exactSum = false;
+    }
+}
+
+Console.WriteLine(ExactNumber());
